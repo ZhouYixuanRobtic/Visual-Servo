7822eff1e3694d70e621b7238f0f8eddc518cb00
YX.E.Z <zhouyixuan29@gmail.com>
2019-05-16 19:49:03 +0800

    change c like code to a c++ code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
+/*
+    visual_servo
+    TO DO:  Add BREAK statement in go_search;
+            Remove global variable Tags_detected in go_servo;
+*/
 #include <iostream>
 
 #include "Servo.h"
-#include "camera.h"
 #include <unistd.h>
 #include <ros/ros.h>
 #include <ros/forwards.h>
@@ -46,6 +50,177 @@ pthread_t camera_id;
 void *camera_thread(void *data);
 void ImageCallback(const sensor_msgs::ImageConstPtr& msg);
 
+class Manipulator
+{
+    private:
+        const std::string PLANNING_GROUP = "manipulator";
+        moveit::planning_interface::MoveGroupInterface *move_group;
+        moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+        const robot_state::JointModelGroup* joint_model_group;
+
+        geometry_msgs::PoseStamped current_pose;
+        Eigen::Affine3d Trans_W2E,Trans_W2EP;
+
+    public:
+        double radius=0.2;
+        Manipulator();
+        virtual ~Manipulator();
+        void add_planning_constraint();
+        void go_up(double goal_tolerance=0.001,double vector_scale=1.0);
+        void go_search(double search_angle=M_PI,double vector_scale=0.05);
+        bool go_servo(Eigen::Affine3d ExpectMatrix,Eigen::Affine3d Trans_E2C,double goal_tolerance=0.02,double vector_scale=1.0);
+        void go_cut(double vector_scale=0.1);
+};
+
+Manipulator::Manipulator()
+{
+    move_group = new moveit::planning_interface::MoveGroupInterface(PLANNING_GROUP);
+    joint_model_group = move_group->getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+    ROS_INFO_NAMED("Visual Servo", "Reference frame: %s", move_group->getPlanningFrame().c_str());
+    ROS_INFO_NAMED("Visual Servo", "End effector link: %s", move_group->getEndEffectorLink().c_str());
+}
+Manipulator::~Manipulator()
+{
+    delete move_group;
+}
+void Manipulator::add_planning_constraint()
+{
+    vector<moveit_msgs::CollisionObject> objects;
+    //添加地面以限制规划
+    moveit_msgs::CollisionObject collision_object;
+    collision_object.header.frame_id = move_group->getPlanningFrame();
+    collision_object.id = "ground";
+    shape_msgs::Plane plane;
+    plane.coef={0,0,1,0};
+    geometry_msgs::Pose object_pose;
+    object_pose.position.x=0;
+    object_pose.position.y=0;
+    object_pose.position.z=-0.2;
+    object_pose.orientation.w=1.0;
+    collision_object.planes.push_back(plane);
+    collision_object.plane_poses.push_back(object_pose);
+    collision_object.operation = collision_object.ADD;
+    objects.push_back(collision_object);
+
+    //添加柱子以限制规划
+    moveit_msgs::CollisionObject collision_object1;
+    collision_object1.header.frame_id = move_group->getPlanningFrame();
+    collision_object1.id = "Heava";
+    shape_msgs::SolidPrimitive primitive;
+    primitive.type = primitive.CYLINDER;
+    primitive.dimensions.resize(2);
+    primitive.dimensions[0] = 5;
+    primitive.dimensions[1] = radius;
+    object_pose.position.y=0.7;
+    collision_object1.primitives.push_back(primitive);
+    collision_object1.primitive_poses.push_back(object_pose);
+    collision_object1.operation = collision_object.ADD;
+    objects.push_back(collision_object1);
+
+    planning_scene_interface.addCollisionObjects(objects);
+}
+void Manipulator::go_up(double goal_tolerance,double vector_scale)
+{
+    move_group->setGoalTolerance(goal_tolerance);
+    move_group->setMaxVelocityScalingFactor(vector_scale);
+    move_group->setNamedTarget("up");
+    move_group->move();
+    sleep(2);
+}
+void Manipulator::go_search(double search_angle,double vector_scale)
+{
+    //旋转关节以搜索tag
+    vector<double> goal;
+    goal={-0.882107921963, -1.42994360967, -0.407164187247, -1.59284015911, 0.902555146013-M_PI/2, 0.182040936536};
+    move_group->setJointValueTarget(goal);
+    move_group->move();
+    sleep(2);
+    goal[4]+=search_angle;
+    move_group->setJointValueTarget(goal);
+    move_group->setMaxVelocityScalingFactor(vector_scale);
+    move_group->asyncMove();
+    while(Tags_detected.empty())
+    {
+        //ROS_INFO("Searching tag!!!!!");
+    }
+    move_group->stop();
+}
+
+bool Manipulator::go_servo(Eigen::Affine3d ExpectMatrix,Eigen::Affine3d Trans_E2C,double goal_tolerance,double vector_scale)
+{
+    current_pose=move_group->getCurrentPose();
+    Eigen::fromMsg(current_pose.pose,Trans_W2E);
+    double error=100;
+    Destination_t EndDestination;
+    //再检查一次
+    move_group->setGoalTolerance(goal_tolerance);
+    move_group->setMaxVelocityScalingFactor(vector_scale);
+
+    while(error>0.02&&!Tags_detected.empty())
+    {
+        EndDestination=astra.GetCameraDestination(Tags_detected[0].Trans_C2T,Trans_E2C,ExpectMatrix);
+        error=EndDestination.error;
+        Trans_W2EP=Trans_W2E*EndDestination.EE_Motion;
+        move_group->setPoseTarget(Trans_W2EP);
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+        bool success = (move_group->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+        if (success)
+        {
+            ROS_INFO("Reachable");
+            move_group->execute(my_plan);
+            sleep(2);
+        }
+        else
+        {
+            ROS_ERROR("Failed");
+            return false;
+        }
+        current_pose=move_group->getCurrentPose();
+        Eigen::fromMsg(current_pose.pose,Trans_W2E);
+    }
+    return true;
+}
+void Manipulator::go_cut(double vector_scale)
+{
+    geometry_msgs::Pose target_pose3 = move_group->getCurrentPose().pose;
+    std::vector<geometry_msgs::Pose> waypoints;
+
+    double init_th=asin(target_pose3.position.x/radius);
+    double init_y=target_pose3.position.y+radius*cos(init_th);
+
+    for(double th=0.0;th<M_PI/3;th+=0.1)
+    {
+        target_pose3.position.x = radius*sin(init_th+th);
+        target_pose3.position.y = init_y-radius*cos(init_th-th) ;
+        target_pose3.position.z -=0.01;
+        waypoints.push_back(target_pose3);
+    }
+    move_group->allowReplanning(true);
+    move_group->setMaxVelocityScalingFactor(vector_scale);
+    move_group->setStartStateToCurrentState();
+    moveit_msgs::RobotTrajectory trajectory;
+    double fraction=0.0;
+    int attempts=0;
+    while(fraction<1.0&&attempts<100)
+    {
+        fraction = move_group->computeCartesianPath(waypoints, 0.01, 0.0, trajectory);
+        attempts++;
+        if(attempts%10==0)
+            ROS_INFO("Still trying after %d attempts",attempts);
+    }
+    ROS_INFO_NAMED("visual servo", "Visualizing plan 4 (Cartesian path) (%.2f%% acheived)", fraction * 100.0);
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+    if(fraction<1.0)
+        ROS_INFO("NO Trajectory");
+    else
+    {
+        my_plan.trajectory_=trajectory;
+        move_group->execute(my_plan);
+        sleep(2);
+    }
+}
+
+
 int main(int argc, char** argv)
 {
   
@@ -66,147 +241,19 @@ int main(int argc, char** argv)
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
-  
-    static const std::string PLANNING_GROUP = "manipulator";
-    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-    const robot_state::JointModelGroup* joint_model_group =
-            move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+    Manipulator robot_manipulator;
 
-    ROS_INFO_NAMED("Visual Servo", "Reference frame: %s", move_group.getPlanningFrame().c_str());
-    ROS_INFO_NAMED("Visual Servo", "End effector link: %s", move_group.getEndEffectorLink().c_str());
-
-
-    vector<moveit_msgs::CollisionObject> objects;
-    //添加地面以限制规划
-    moveit_msgs::CollisionObject collision_object;
-    collision_object.header.frame_id = move_group.getPlanningFrame();
-    collision_object.id = "ground";
-    shape_msgs::Plane plane;
-    plane.coef={0,0,1,0};
-    geometry_msgs::Pose object_pose;
-    object_pose.position.x=0;
-    object_pose.position.y=0;
-    object_pose.position.z=-0.2;
-    object_pose.orientation.w=1.0;
-    collision_object.planes.push_back(plane);
-    collision_object.plane_poses.push_back(object_pose);
-    collision_object.operation = collision_object.ADD;
-    objects.push_back(collision_object);
-    //添加柱子以限制规划
-    moveit_msgs::CollisionObject collision_object1;
-    collision_object1.header.frame_id = move_group.getPlanningFrame();
-    collision_object1.id = "Heava";
-    shape_msgs::SolidPrimitive primitive;
-    primitive.type = primitive.CYLINDER;
-    primitive.dimensions.resize(2);
-    primitive.dimensions[0] = 5;
-    primitive.dimensions[1] = 0.2;
-    object_pose.position.y=0.7;
-    collision_object1.primitives.push_back(primitive);
-    collision_object1.primitive_poses.push_back(object_pose);
-    collision_object1.operation = collision_object.ADD;
-    objects.push_back(collision_object1);
-
-    planning_scene_interface.addCollisionObjects(objects);
-
-    //去往竖直位
-    move_group.setNamedTarget("up");
-    move_group.move();
-    sleep(2);
-    
-    geometry_msgs::PoseStamped current_pose=move_group.getCurrentPose();
-    Eigen::fromMsg(current_pose.pose,Trans_W2E);
+    robot_manipulator.add_planning_constraint();
+    robot_manipulator.go_up();
     if(Tags_detected.empty())
-    {
-        //旋转关节以搜索tag
-        vector<double> goal;
-        goal={-0.882107921963, -1.42994360967, -0.407164187247, -1.59284015911, 0.902555146013-M_PI/2, 0.182040936536};
-        move_group.setJointValueTarget(goal);
-        move_group.move();
-        sleep(2);
-        goal[4]+=M_PI;
-        move_group.setJointValueTarget(goal);
-        move_group.setMaxVelocityScalingFactor(0.05);
-        move_group.asyncMove();
-        while(Tags_detected.empty())
-        {
-            //ROS_INFO("Searching tag!!!!!");
-        }
-        move_group.stop();
-    }
-  
-    //Servo
-    double error=100;
-    Destination_t EndDestination;
-    //再检查一次
-    move_group.setGoalTolerance(0.02);
-    move_group.setMaxVelocityScalingFactor(1.0);
+        robot_manipulator.go_search();
 
-    while(error>0.02&&!Tags_detected.empty())
-    {
-        EndDestination=astra.GetCameraDestination(Tags_detected[0].Trans_C2T,Trans_E2C,ExpectMatrix);        
-        error=EndDestination.error;
-        Trans_W2EP=Trans_W2E*EndDestination.EE_Motion;
-        move_group.setPoseTarget(Trans_W2EP);
-        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-        bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-        if (success)
-        {
-            ROS_INFO("Reachable");
-            move_group.execute(my_plan);
-            sleep(2);
-        }
-        else
-        {
-            ROS_ERROR("Failed");
-            break;
-        }
-        current_pose=move_group.getCurrentPose();
-        Eigen::fromMsg(current_pose.pose,Trans_W2E);
-    }
-    
-    //按轨迹操作
-    geometry_msgs::Pose target_pose3 = move_group.getCurrentPose().pose;
-    std::vector<geometry_msgs::Pose> waypoints;
-    
-    double init_th=asin(target_pose3.position.x/0.2);
-    double init_y=target_pose3.position.y+0.2*cos(init_th);
-    for(double th=0.0;th<M_PI/3;th+=0.1)
-    {
-        target_pose3.position.x = 0.2*sin(init_th+th);
-        target_pose3.position.y = init_y-0.2*cos(init_th-th) ;
-        target_pose3.position.z -=0.01;
-        waypoints.push_back(target_pose3);
-    }
-    move_group.allowReplanning(true);
-    move_group.setMaxVelocityScalingFactor(0.1);
-    move_group.setStartStateToCurrentState();
-    moveit_msgs::RobotTrajectory trajectory;
-    double fraction=0.0;
-    int attempts=0;
-    while(fraction<1.0&&attempts<100)
-    {
-        fraction = move_group.computeCartesianPath(waypoints, 0.01, 0.0, trajectory);
-        attempts++;
-        if(attempts%10==0)
-            ROS_INFO("Still trying after %d attempts",attempts);
-    }
-    ROS_INFO_NAMED("visual servo", "Visualizing plan 4 (Cartesian path) (%.2f%% acheived)", fraction * 100.0);
-    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-    if(fraction<1.0)
-        ROS_INFO("NO Trajectory");
-    else
-    {
-        my_plan.trajectory_=trajectory; 
-        move_group.execute(my_plan);
-        sleep(2);
-    }
-    //机械臂回位
-    move_group.setGoalTolerance(0.001);
-    move_group.setMaxVelocityScalingFactor(1.0);
-    move_group.setNamedTarget("up");
-    move_group.move();
+    bool success=robot_manipulator.go_servo(ExpectMatrix,Trans_E2C);
+    if(success)
+        robot_manipulator.go_cut();
+
+    robot_manipulator.go_up();
+
     ros::waitForShutdown();
     return 0;
 
