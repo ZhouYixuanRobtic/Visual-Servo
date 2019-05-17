382b5d12b870252fb50bdb7f54148d3e5e3ed2c5
YX.E.Z <zhouyixuan29@gmail.com>
2019-05-17 20:35:00 +0800

    search function changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 /*
     visual_servo
-    TO DO:  Add BREAK statement in go_search;
-            Test the search function;
-            Add re-search function in go_servo;
+    TO DO:  MOVE TAG DOWN
 */
 #include <iostream>
 
@@ -32,7 +30,7 @@
 #include <tf2_eigen/tf2_eigen.h>
 
 #include "ros/callback_queue.h"
-#include  <math.h>
+#include<math.h>
 #include "visual_servo/TagDetection_msg.h"
 #include "visual_servo/TagsDetection_msg.h"
 
@@ -53,33 +51,31 @@ void ImageCallback(const sensor_msgs::ImageConstPtr& msg);
 
 class Manipulator
 {
-    private:
-        const std::string PLANNING_GROUP = "manipulator";
-        moveit::planning_interface::MoveGroupInterface *move_group;
-        moveit::planning_interface::PlanningSceneInterface *planning_scene_interface;
-        const robot_state::JointModelGroup* joint_model_group;
+private:
+    const std::string PLANNING_GROUP = "manipulator";
+    moveit::planning_interface::MoveGroupInterface *move_group;
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+    const robot_state::JointModelGroup* joint_model_group;
 
-        geometry_msgs::PoseStamped current_pose;
-        Eigen::Affine3d Trans_W2E,Trans_W2EP;
-        geometry_msgs::Pose target_pose;
-        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+    geometry_msgs::PoseStamped current_pose;
+    Eigen::Affine3d Trans_W2E,Trans_W2EP;
 
-    public:
-        double radius=0.2;
-        Manipulator();
-        virtual ~Manipulator();
-        void add_planning_constraint();
-        void go_up(double goal_tolerance=0.001,double velocity_scale=1.0);
-        bool get_task_plan(string task_name,double task_parameter);
-        bool go_search(double search_angle=M_PI,double velocity_scale=0.05,double goal_tolerance=0.001);
-        bool go_servo(Eigen::Affine3d ExpectMatrix,Eigen::Affine3d Trans_E2C,double goal_tolerance=0.02,double velocity_scale=1.0);
-        void go_cut(double goal_tolerance=0.02,double velocity_scale=0.1);
+
+public:
+    double radius=0.2;
+    Manipulator();
+    virtual ~Manipulator();
+    void add_planning_constraint();
+    void go_up(double goal_tolerance=0.001,double velocity_scale=1.0);
+    bool go_search_once(string search_type,double search_angle=M_PI,double velocity_scale=0.05,double goal_tolerance=0.001);
+    bool go_search(double search_angle=M_PI,double velocity_scale=0.05,double goal_tolerance=0.001);
+    bool go_servo(Eigen::Affine3d ExpectMatrix,Eigen::Affine3d Trans_E2C,double goal_tolerance=0.02,double velocity_scale=1.0);
+    void go_cut(double velocity_scale=0.1);
 };
 
 Manipulator::Manipulator()
 {
     move_group = new moveit::planning_interface::MoveGroupInterface(PLANNING_GROUP);
-    planning_scene_interface = new moveit::planning_interface::PlanningSceneInterface;
     joint_model_group = move_group->getCurrentState()->getJointModelGroup(PLANNING_GROUP);
     ROS_INFO_NAMED("Visual Servo", "Reference frame: %s", move_group->getPlanningFrame().c_str());
     ROS_INFO_NAMED("Visual Servo", "End effector link: %s", move_group->getEndEffectorLink().c_str());
@@ -87,7 +83,6 @@ Manipulator::Manipulator()
 Manipulator::~Manipulator()
 {
     delete move_group;
-    delete planning_scene_interface;
 }
 void Manipulator::add_planning_constraint()
 {
@@ -123,7 +118,7 @@ void Manipulator::add_planning_constraint()
     collision_object1.operation = collision_object.ADD;
     objects.push_back(collision_object1);
 
-    planning_scene_interface->addCollisionObjects(objects);
+    planning_scene_interface.addCollisionObjects(objects);
 }
 void Manipulator::go_up(double goal_tolerance,double velocity_scale)
 {
@@ -133,73 +128,52 @@ void Manipulator::go_up(double goal_tolerance,double velocity_scale)
     move_group->move();
     sleep(2);
 }
-
-bool Manipulator::get_task_plan(string task_name,double task_parameter)
-{
-    geometry_msgs::Pose target_pose = move_group->getCurrentPose().pose;
-    std::vector<geometry_msgs::Pose> waypoints;
-    if(task_name=="cut")
-    {
-        double init_th = asin(target_pose.position.x / radius);
-        double init_y = target_pose.position.y + radius * cos(init_th);
-
-        for (double th = 0.0; th < task_parameter; th += 0.1)
-        {
-            target_pose.position.x = radius * sin(init_th + th);
-            target_pose.position.y = init_y - radius * cos(init_th - th);
-            target_pose.position.z -= 0.01;
-            waypoints.push_back(target_pose);
-        }
-    }
-    else if (task_name=="back")
-    {
-        double theta = atan(target_pose.position.y / target_pose.position.x);
-        for (int counter = 0; counter < (int) task_parameter*100; ++counter)
-        {
-            target_pose.position.x -= 0.01 * cos(theta);
-            target_pose.position.y -= 0.01 * sin(theta);
-            waypoints.push_back(target_pose);
-        }
-    }
-
-    move_group->allowReplanning(true);
-    move_group->setStartStateToCurrentState();
-    moveit_msgs::RobotTrajectory trajectory;
-    double fraction=0.0;
-    int attempts=0;
-    while(fraction<1.0&&attempts<100)
-    {
-        fraction = move_group->computeCartesianPath(waypoints, 0.01, 0.0, trajectory);
-        attempts++;
-        if(attempts%10==0)
-            ROS_INFO("Still trying after %d attempts",attempts);
-    }
-    if(fraction<1.0)
-        return false;
-    else
-    {
-        move_group->setStartStateToCurrentState();
-        my_plan.trajectory_=trajectory;
-    }
-    return true;
-}
-bool Manipulator::go_search(double search_angle,double velocity_scale,double goal_tolerance)
+bool Manipulator::go_search_once(string search_type,double search_angle,double velocity_scale,double goal_tolerance)
 {
     //旋转关节以搜索tag
-    if(get_task_plan("back",0.2));
+    move_group->setGoalTolerance(goal_tolerance);
+    vector<double> goal;
+    if(search_type=="flat")
+        goal=move_group->getCurrentJointValues();
+    else if(search_type=="look up")
+        goal={-0.801147672791,-1.46294256182,-0.503654629856,-0.851993156669,0.827502945626,-0.222524404845};
+    else if(search_type=="look down")
+        goal={-0.961324653895,-1.47830998925,-0.343705555765,-1.60330331121,0.980292289712,0.161102308097};
+    else
     {
-        move_group->setGoalTolerance(goal_tolerance);
-        move_group->setMaxVelocityScalingFactor(velocity_scale);
-
-        move_group->asyncExecute(my_plan);
-        while(Tags_detected.empty())
-        {
-            //TO DO;
-        }
-        move_group->stop();
+        ROS_ERROR("Wrong search type!!!!");
+        return false;
     }
+    move_group->setMaxVelocityScalingFactor(1.0);
+    goal[4]-=search_angle/2;
+    move_group->setJointValueTarget(goal);
+    move_group->move();
+    sleep(2);
+    goal[4]+=search_angle;
+    move_group->setJointValueTarget(goal);
+    move_group->setMaxVelocityScalingFactor(velocity_scale);
+    move_group->asyncMove();
+    vector<double> current_joints;
+    while(Tags_detected.empty())
+    {
+        //ROS_INFO("Searching tag!!!!!");
+        current_joints=move_group->getCurrentJointValues();
+        std::vector<double>	auxiliary;
 
+        std::transform (current_joints.begin(), current_joints.end(), goal.begin(), std::back_inserter(auxiliary),//
+                        [](double element1, double element2) {return pow((element1-element2),2);});
+        auxiliary.shrink_to_fit();
+        if(sqrt(std::accumulate(auxiliary.begin(), auxiliary.end(), 0.0))<=goal_tolerance*2)
+            return false;
+    }
+    move_group->stop();
     return true;
+}
+bool Manipulator::go_search(double search_angle, double velocity_scale, double goal_tolerance)
+{
+    return  go_search_once("flat",search_angle,velocity_scale,goal_tolerance) ||
+            go_search_once("look down",search_angle,velocity_scale,goal_tolerance) ||
+            go_search_once("look up",search_angle,velocity_scale,goal_tolerance);
 }
 
 bool Manipulator::go_servo(Eigen::Affine3d ExpectMatrix,Eigen::Affine3d Trans_E2C,double goal_tolerance,double velocity_scale)
@@ -212,52 +186,80 @@ bool Manipulator::go_servo(Eigen::Affine3d ExpectMatrix,Eigen::Affine3d Trans_E2
     move_group->setGoalTolerance(goal_tolerance);
     move_group->setMaxVelocityScalingFactor(velocity_scale);
 
-    while(error>goal_tolerance&&!Tags_detected.empty())
+    while(error>0.02)
     {
-        EndDestination=astra.GetCameraDestination(Tags_detected[0].Trans_C2T,Trans_E2C,ExpectMatrix);
-        error=EndDestination.error;
-        Trans_W2EP=Trans_W2E*EndDestination.EE_Motion;
-        move_group->setPoseTarget(Trans_W2EP);
+        if(!Tags_detected.empty()||go_search())
+        {
+            EndDestination=astra.GetCameraDestination(Tags_detected[0].Trans_C2T,Trans_E2C,ExpectMatrix);
+            error=EndDestination.error;
+            Trans_W2EP=Trans_W2E*EndDestination.EE_Motion;
+            move_group->setPoseTarget(Trans_W2EP);
+            moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+            bool success = (move_group->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+            if (success)
+            {
+                ROS_INFO("Reachable");
+                move_group->execute(my_plan);
+                sleep(2);
+            }
+            else
+            {
+                ROS_ERROR("Failed");
+                return false;
+            }
+            current_pose=move_group->getCurrentPose();
+            Eigen::fromMsg(current_pose.pose,Trans_W2E);
+        }
 
-        bool success = (move_group->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-        if (success)
-        {
-            ROS_INFO("Reachable");
-            move_group->execute(my_plan);
-            sleep(2);
-        }
-        else
-        {
-            ROS_ERROR("Failed");
-            return false;
-        }
-        current_pose=move_group->getCurrentPose();
-        Eigen::fromMsg(current_pose.pose,Trans_W2E);
     }
     return true;
 }
-void Manipulator::go_cut(double goal_tolerance, double velocity_scale)
+void Manipulator::go_cut(double velocity_scale)
 {
-    if(get_task_plan("cut",M_PI/3))
+    geometry_msgs::Pose target_pose3 = move_group->getCurrentPose().pose;
+    std::vector<geometry_msgs::Pose> waypoints;
+
+    double init_th=asin(target_pose3.position.x/radius);
+    double init_y=target_pose3.position.y+radius*cos(init_th);
+
+    for(double th=0.0;th<M_PI/3;th+=0.1)
     {
-        move_group->setGoalTolerance(goal_tolerance);
-        move_group->setMaxVelocityScalingFactor(velocity_scale);
+        target_pose3.position.x = radius*sin(init_th+th);
+        target_pose3.position.y = init_y-radius*cos(init_th-th) ;
+        target_pose3.position.z -=0.01;
+        waypoints.push_back(target_pose3);
+    }
+    move_group->allowReplanning(true);
+    move_group->setMaxVelocityScalingFactor(velocity_scale);
+    move_group->setStartStateToCurrentState();
+    moveit_msgs::RobotTrajectory trajectory;
+    double fraction=0.0;
+    int attempts=0;
+    while(fraction<1.0&&attempts<100)
+    {
+        fraction = move_group->computeCartesianPath(waypoints, 0.01, 0.0, trajectory);
+        attempts++;
+        if(attempts%10==0)
+            ROS_INFO("Still trying after %d attempts",attempts);
+    }
+    ROS_INFO_NAMED("visual servo", "Visualizing plan 4 (Cartesian path) (%.2f%% acheived)", fraction * 100.0);
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+    if(fraction<1.0)
+        ROS_INFO("NO Trajectory");
+    else
+    {
+        my_plan.trajectory_=trajectory;
         move_group->execute(my_plan);
         sleep(2);
     }
-    else
-    {
-        ROS_INFO("CUT LOSE!!!!!");
-    }
-
 }
 
 
 int main(int argc, char** argv)
 {
-  
+
     Eigen::Affine3d ExpectMatrix,Trans_E2C;
-  
+
     ExpectMatrix.matrix() << 1,0,0,0,
             0,1,0,0,
             0,0,1,0.2,
@@ -266,7 +268,7 @@ int main(int argc, char** argv)
             -1,0,0,0,
             0,-1,0,0.0675,
             0,0,0,1;
-  
+
     ros::init(argc, argv, "visual_servo");
     ros::NodeHandle n;
     pthread_create(&camera_id, NULL, camera_thread, NULL);
@@ -276,7 +278,6 @@ int main(int argc, char** argv)
     Manipulator robot_manipulator;
 
     robot_manipulator.add_planning_constraint();
-
     robot_manipulator.go_up();
     if(Tags_detected.empty())
     {
@@ -302,6 +303,7 @@ int main(int argc, char** argv)
             robot_manipulator.go_cut();
         robot_manipulator.go_up();
     }
+
     ros::waitForShutdown();
     return 0;
 
@@ -316,23 +318,23 @@ void *camera_thread(void *data)
     ros::Rate loop_rate(15);
     while(ros::ok())
     {
+
         if(!Tags_detected.empty())
         {
             visual_servo::TagsDetection_msg TagsDetection;
             visual_servo::TagDetection_msg TagDetection;
             TagsDetection.header.frame_id="camera_link";
-            TagsDetection.header.stamp=ros::Time::now();
             for(auto & tag_detected : Tags_detected )
             {
                 TagDetection.id = tag_detected.id;
                 TagDetection.pose = Eigen::toMsg(tag_detected.Trans_C2T);
-                TagDetection.PixelCoef = tag_detected.PixelCoef;    
+                TagDetection.PixelCoef = tag_detected.PixelCoef;
                 TagDetection.center.x = tag_detected.Center.x;
                 TagDetection.center.y = tag_detected.Center.y;
                 TagsDetection.tags_information.push_back(TagDetection);
-            } 
+            }
             tag_pub.publish(TagsDetection);
-        }   
+        }
         ros::spinOnce();
         loop_rate.sleep();
     }
